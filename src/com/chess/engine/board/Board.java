b757390dc877a1b298561dbcfc884ee8baec1688
harischatzianastasiou <harischati@tutanota.com>
2024-10-24 01:53:11 +0300

    Added Knight implementation

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -2,4 +2,20 @@ package com.chess.engine.board;
 
 public class Board {
 
+	public Tile getTile(final int candidateDestinationCoordinate) {
+		return null;
+	}
+	
+	public static boolean isValidTile(final int tileCoordinate) {
+		
+		boolean isValidTile = false;
+		
+		if(tileCoordinate >= 0 && tileCoordinate < 64)
+			isValidTile = true;
+		
+        return isValidTile;
+	}
+	
+	
+
 }
