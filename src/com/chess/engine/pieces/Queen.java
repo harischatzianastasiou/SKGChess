9b004bc95db857a52972f1b9e2893192e215c63b
Haris Chatzianastasiou <harischatzi@tutanota.com>
2024-11-01 21:02:12 +0200

    fix

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -2,14 +2,12 @@ package com.chess.engine.pieces;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.board.Tile;
-import com.chess.engine.pieces.Rook;
 import com.google.common.collect.ImmutableList;
 
 public class Queen extends Piece {
