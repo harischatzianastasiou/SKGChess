d5eaa8eca26410063219ce1188ff37fee8b9818a
Haris Chatzianastasiou <harischatzi@tutanota.com>
2024-11-28 21:27:20 +0200

    Queen

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -5,36 +5,103 @@ import java.util.Collection;
 import java.util.List;
 
 import com.chess.engine.Alliance;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Move.CapturingMove;
+import com.chess.engine.board.Move.NonCapturingMove;
 import com.chess.engine.board.Tile;
 import com.google.common.collect.ImmutableList;
 
 public class Queen extends Piece {
+    
+    private static final int[] ROOK_MOVE_OFFSETS = { -8, -1, 1, 8 };
+    private static final int[] BISHOP_MOVE_OFFSETS = { -9, -7, 7, 9 };
+    private static final int MAX_SQUARES_MOVED = 7;
 
-	public Queen(final int pieceCoordinate, final Alliance pieceAlliance) {
+    public Queen(final int pieceCoordinate, final Alliance pieceAlliance) {
         super(PieceSymbol.QUEEN, pieceCoordinate, pieceAlliance, true);
-	}
-	
-	public Queen(final int pieceCoordinate, final Alliance pieceAlliance, final boolean isFirstMove) {
+    }
+
+    public Queen(final int pieceCoordinate, final Alliance pieceAlliance, final boolean isFirstMove) {
         super(PieceSymbol.QUEEN, pieceCoordinate, pieceAlliance, isFirstMove);
-	}
-	
+    }
+
     @Override
     public String toString() {
         return this.pieceSymbol.toString();
     }
-    
-	@Override
-	public Collection<Move> calculateMoves(final List<Tile> boardTiles, final Alliance currentPlayer) {
-        final Collection<Move> equivalentRookMoves = new Rook(this.pieceCoordinate, this.pieceAlliance).calculateMoves(boardTiles, currentPlayer);
-        final Collection<Move> equivalentBishopMoves = new Bishop(this.pieceCoordinate, this.pieceAlliance).calculateMoves(boardTiles, currentPlayer);
-        final List<Move> legalMoves = new ArrayList<>(equivalentRookMoves);
-        legalMoves.addAll(equivalentBishopMoves);
+
+    @Override
+    public Collection<Move> calculateMoves(final List<Tile> boardTiles) {
+        final List<Move> legalMoves = new ArrayList<>();
+        legalMoves.addAll(calculateMovesForRook(boardTiles));
+        legalMoves.addAll(calculateMovesForBishop(boardTiles));
         return ImmutableList.copyOf(legalMoves);
-	}
-	
+    }
+
+    public Collection<Move> calculateMovesForRook(final List<Tile> boardTiles) {
+		final List<Move> legalMoves = new ArrayList<>();
+		int candidateDestinationCoordinate;
+		for (final int candidateOffset : ROOK_MOVE_OFFSETS) {
+			for(int squaresMoved=1; squaresMoved <= MAX_SQUARES_MOVED; squaresMoved++ ) {
+				int total_offset = candidateOffset * squaresMoved;
+				candidateDestinationCoordinate = this.pieceCoordinate + total_offset;
+	            if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+	            	int rankDifference = BoardUtils.getCoordinateRankDifference(candidateDestinationCoordinate, this.pieceCoordinate);
+                    int fileDifference = BoardUtils.getCoordinateFileDifference(candidateDestinationCoordinate, this.pieceCoordinate);
+                    if (rankDifference == 0 || fileDifference == 0) {
+                        final Tile candidateDestinationTile = boardTiles.get(candidateDestinationCoordinate);
+                        if (!candidateDestinationTile.isTileOccupied()) {
+	            		    legalMoves.add(new NonCapturingMove(boardTiles,this.pieceCoordinate, candidateDestinationCoordinate, this));
+		            	}else {
+		            		final Piece pieceOnCandidateDestinationTile = candidateDestinationTile.getPiece();
+		            		final Alliance allianceOfPieceOnCandidateDestinationTile = pieceOnCandidateDestinationTile.getPieceAlliance();
+		            		if( this.pieceAlliance != allianceOfPieceOnCandidateDestinationTile){
+		                        legalMoves.add(new CapturingMove(boardTiles,this.pieceCoordinate, candidateDestinationCoordinate, this, pieceOnCandidateDestinationTile));
+		                    }
+		            		break;//if there is a piece in the direction that bishop can move, stop further checking in this direction.
+		            	}
+	            	}
+	            }
+			}
+		}
+		return ImmutableList.copyOf(legalMoves);
+    }
+
+    public Collection<Move> calculateMovesForBishop(final List<Tile> boardTiles) {
+		final List<Move> legalMoves = new ArrayList<>();
+		int candidateDestinationCoordinate;
+		for (final int candidateOffset : BISHOP_MOVE_OFFSETS) {
+			for(int squaresMoved=1; squaresMoved <= MAX_SQUARES_MOVED; squaresMoved++ ) {
+				int total_offset = candidateOffset * squaresMoved;
+				candidateDestinationCoordinate = this.pieceCoordinate + total_offset;
+				if (BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
+					final Tile candidateDestinationTile = boardTiles.get(candidateDestinationCoordinate);	          
+					final Alliance allianceOfCandidateDestinationTile = candidateDestinationTile.getTileAlliance();
+	            	final Tile currentTile = boardTiles.get(pieceCoordinate);	
+	            	final Alliance allianceOfCurrentTile = currentTile.getTileAlliance(); 
+	            	if (allianceOfCandidateDestinationTile == allianceOfCurrentTile) {
+	            		if (!candidateDestinationTile.isTileOccupied()) { 
+	            		    	legalMoves.add(new NonCapturingMove(boardTiles,this.pieceCoordinate, candidateDestinationCoordinate, this));
+		            	}else {
+		            		final Piece pieceOnCandidateDestinationTile = candidateDestinationTile.getPiece();
+		            		final Alliance allianceOfPieceOnCandidateDestinationTile = pieceOnCandidateDestinationTile.getPieceAlliance();
+		            		if( this.pieceAlliance != allianceOfPieceOnCandidateDestinationTile ){
+		                        legalMoves.add(new CapturingMove(boardTiles,this.pieceCoordinate, candidateDestinationCoordinate, this, pieceOnCandidateDestinationTile));
+		                    }
+		            		break;//if there is a piece in the direction that bishop can move, stop further checking in this direction.
+		            	}
+	            	}
+	            	else break; //if the current vector does not apply for the pieceTile(eg for tiles that are on 1st or 8th rank), stop further checking in this direction.
+	            }
+	            else break;//If the candidateTargetCoordinate is out of boundaries, stop further checking in this direction.
+			} 
+		}
+		return ImmutableList.copyOf(legalMoves);
+    }
+
     @Override
     public Piece movePiece(int destinationCoordinate) {
-        return new Queen(destinationCoordinate, this.getPieceAlliance(),false);
+        return new Queen(destinationCoordinate, this.getPieceAlliance(), false);
     }
 }
