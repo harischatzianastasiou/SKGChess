bd34ec32b5f79558baa376c0deeca86452c4bbb2
Haris Chatzianastasiou <harischatzi@tutanota.com>
2025-04-16 12:41:25 +0300

    Design

--- src/main/java/com/chess/controller/MainController.java
+++ src/main/java/com/chess/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.chess.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +9,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.chess.dto.rest.response.GameDTO;
 import com.chess.model.entity.Game;
+import com.chess.model.entity.User;
 import com.chess.service.GameService;
+import com.chess.service.UserService;
 
 
 @Controller // GET requests to /req/login and /req/signup return HTML templates AS @Controller is for views
 public class MainController {
 
     private final GameService gameService;
+    private final UserService userService;
 
-    public MainController(GameService gameService) {
+    public MainController(GameService gameService, UserService userService) {
         this.gameService = gameService;
+        this.userService = userService;
     }
 
     @GetMapping("/login")
@@ -38,9 +44,23 @@ public class MainController {
     public String getGame(@PathVariable String gameId, Model model) {
         // Get the game data from service
         Game game = gameService.getGameById(gameId);
-        // Add the game data to the model so it's available in the template
-        model.addAttribute("game", GameDTO.fromGame(game));
-        // Return the template name (Spring will look for game.html in templates folder)
+        GameDTO gameDTO = GameDTO.fromGame(game);
+        
+        // Get current user information
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String username = auth.getName();
+        User currentUser = userService.getUserByUsername(username);
+        
+        // Add all necessary attributes to the model
+        model.addAttribute("gameId", gameDTO.getId());
+        model.addAttribute("whitePlayerId", gameDTO.getWhitePlayerId());
+        model.addAttribute("blackPlayerId", gameDTO.getBlackPlayerId());
+        model.addAttribute("whitePlayerUsername", gameDTO.getWhitePlayerUsername());
+        model.addAttribute("blackPlayerUsername", gameDTO.getBlackPlayerUsername());
+        model.addAttribute("userId", currentUser.getId());
+        model.addAttribute("username", username);
+        
+        // Return the template name
         return "game";
     }
 
