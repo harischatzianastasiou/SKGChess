9f1cebe42d6f2981b63355a935cb0a6dda6f1254
Haris Chatzianastasiou <harischatzi@tutanota.com>
2025-04-25 17:00:15 +0300

    Design

--- target/classes/static/js/modal.js
+++ target/classes/static/js/modal.js
@@ -32,11 +32,11 @@ function createLoginModal() {
                     <form id="login-form" class="login-form" action="/login" method="POST">
                         <div class="form-group">
                             <label for="modal-username">Username</label>
-                            <input type="text" id="modal-username" name="username" required>
+                            <input type="text" id="modal-username" name="username" class="modal-username" required>
                         </div>
                         <div class="form-group">
                             <label for="modal-password">Password</label>
-                            <input type="password" id="modal-password" name="password" required>
+                            <input type="password" id="modal-password" name="password" class="modal-password" required>
                         </div>
                         <div class="remember-me">
                             <input type="checkbox" id="modal-remember" name="remember">
