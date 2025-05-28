ba47a3669f9b1f68867a8b0fb257d46a0742ec0c
Haris Chatzianastasiou <harischatzi@tutanota.com>
2025-05-29 02:50:04 +0300

    Fixes for prod

--- src/main/java/com/chess/util/Sounduser.java
+++ src/main/java/com/chess/util/Sounduser.java
@@ -1,7 +1,7 @@
 package com.chess.util;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -9,18 +9,29 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
 public class Sounduser {
-    private static final String MOVE_SOUND_PATH = "sounds/Move.wav";
-    private static final String CAPTURE_SOUND_PATH = "sounds/Capture.wav";
-    private static final String CHECK_SOUND_PATH = "sounds/Check.wav";
-    private static final String CHECKMATE_SOUND_PATH = "sounds/Checkmate.mp3";
+    private static final String MOVE_SOUND_PATH = "static/sounds/Move.wav";
+    private static final String CAPTURE_SOUND_PATH = "static/sounds/Capture.wav";
+    private static final String CHECK_SOUND_PATH = "static/sounds/Check.wav";
+    private static final String CHECKMATE_SOUND_PATH = "static/sounds/Checkmate.mp3";
     
     private static void playSound(String soundPath) {
         try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(soundPath));
+            // Load the sound file from classpath
+            Resource resource = new ClassPathResource(soundPath);
+            InputStream inputStream = resource.getInputStream();
+            
+            // Create audio input stream
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             clip.start();
+            
+            // Close the input stream
+            inputStream.close();
         } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
